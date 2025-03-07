daaade47c630dc7051dad23e807e83bbf6b5bab9
5takarcoin <5takarcoin@gmail.com>
2025-03-07 21:16:06 +0600

    Before add ghost

---
+++ src/app/getstarted/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [load, setLoad] = useState(false);
+  useEffect(() => {
+    setLoad(true);
+  }, []);
+  return (
+    <div className="m-auto min-h-[900px] aspect-[8/16] flex flex-col items-center h-screen  bg-white shadow-lg overflow-hidden relative">
+      <div
+        className={`${
+          !load ? "translate-y-0 " : "translate-y-[80%] "
+        } bg-black h-11/12 w-full z-100 absolute bottom-0 rounded-t-4xl transition-all duration-500 ease-in flex justify-between items-center flex-col`}
+      >
+        <div className="w-8 h-2 bg-black rounded-full m-auto -mt-4"></div>
+      </div>
+      <div
+        className={` h-full duration-200 w-full ${
+          load ? "bg-white" : "bg-purp"
+        }`}
+      ></div>
+
+      <Link
+        href="/getstarted"
+        className={`z-100 hover:cursor-pointer bg-white text-center p-8  rounded-full  absolute  bottom-0 mb-8 w-4/5 font-bold text-3xl `}
+      >
+        Next
+      </Link>
+
+      <div
+        className={`z-100 flex flex-col items-start justify-between h-4/5  w-full text-2xl absolute text-center duration-200`}
+      >
+        <Image
+          className={`delay-400 w-2/5 mx-auto my-12 transition-scale duration-200 ease-out ${
+            load ? "translate-y-0 opacity-100" : "translate-y-[400%] opacity-0"
+          }`}
+          src="./logo.svg"
+          alt="glass"
+          width={180}
+          height={38}
+          priority
+        />
+        <p className="text-center w-full p-8">I am MyBud</p>
+      </div>
+
+      <div
+        className={`absolute z-[200] top-12 left-8 h-12 w-12 hover:rotate-3 hover:scale-110 `}
+      >
+        <Link href={"/"} className="">
+          <Image
+            className={`delay-300 duration-300   ${
+              load ? "scale-100 rotate-0" : "scale-0 rotate-[720deg]"
+            }`}
+            src="./back.svg"
+            alt="glass"
+            width={180}
+            height={38}
+            priority
+          />
+        </Link>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,98 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [load, setLoad] = useState(false);
+  useEffect(() => {
+    setLoad(true);
+  }, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="m-auto min-h-[900px] aspect-[8/16] flex flex-col items-center h-screen  bg-white shadow-lg overflow-hidden relative">
+      <div
+        className={`${
+          load ? "translate-y-0 opacity-100" : "translate-y-full opacity-0"
+        } bg-pink h-11/12 w-full z-100 absolute bottom-0 rounded-t-4xl transition-all duration-500 ease-in flex justify-between items-center flex-col`}
+      >
+        <div className="w-8 h-2 bg-white rounded-full m-auto -mt-4"></div>
+        <p
+          className={`absolute top-[10%] font-bold text-xl delay-[1100ms] duration-200 ${
+            load ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          Hello this is
+        </p>
+        {/* <Link
+          href="/getstarted"
+          className={`z-11 hover:cursor-pointer bg-white text-center p-8  rounded-full  absolute bottom-0 mb-8 w-4/5 font-bold text-3xl delay-[1100ms] duration-200 ${
+            load ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          Get Started
+        </Link> */}
+        <div
+          className={`space-y-8 delay-[1100ms] duration-200 flex flex-col items-center w-2/5 ${
+            load ? "-translate-y-[40%]" : "translate-y-0"
+          }`}
+        >
+          <div
+            className={`delay-[700ms] duration-200 ${
+              load
+                ? "translate-y-0 opacity-100 "
+                : "-translate-y-[400%] opacity-0 "
+            }`}
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className={` delay-[700ms] w-full duration-200  ${
+                load ? "opacity-100 " : " opacity-0 "
+              }`}
+              src="./logo.svg"
+              alt="glass"
+              width={180}
+              height={38}
+              priority
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <Image
+            className={`delay-500 w-full transition-scale duration-200 ease-out ${
+              load ? "scale-y-100" : "scale-y-0 "
+            }`}
+            src="./glass.svg"
+            alt="glass"
+            width={180}
+            height={38}
+            priority
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="w-[140%] flex justify-center items-center">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            className="w-full  translate-y-1/4"
+            src="/wave.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
+      <div className="bg-purp h-full w-full"></div>
+      <Link
+        href="/getstarted"
+        className={`z-100 hover:cursor-pointer bg-white text-center p-8  rounded-full  absolute  bottom-0 mb-8 w-4/5 font-bold text-3xl delay-[1100ms] duration-200 ${
+          load ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        Get Started
+      </Link>
+      <p
+        className={`z-100 text-2xl absolute bottom-40 text-center delay-[1100ms] duration-200 ${
+          load ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        How are you feeling today?
+      </p>
     </div>
   );
 }
